5d4221ccd91c23ad51133594b1909a3a7f65af96
naval <navalmca2009@gmail.com>
2015-05-12 23:44:28 +0530

    added test file for create dump and restore

--- test/default.js
+++ test/default.js
@@ -1,18 +1,50 @@
 
-	
-	var   Class 		= require('ee-class')
-		, log 			= require('ee-log')
-		, assert 		= require('assert');
 
+var Class = require('ee-class')
+        , log = require('ee-log')
+        , assert = require('assert')
+        , PgTools = require('../');
 
+describe('Create dump sql file', function () {
+    it('Should create dump sql file', function () {
+        var tool = new PgTools();
+        tool.dumpDatabase({
+            host: 'localhost'
+            , port: 5432
+            , user: 'postgres'
+            , password: 'navalarti'
+            , dumpPath: '/home/naval'
+            , database: 'arunav'
+        }, function (error, output, message, dumpFileName) {
+            if (error instanceof Error) {
+                console.log(code);
+            } else {
+                console.log(output);
+                console.log(message);
+                console.log(dumpFileName);
 
-	var Something = require('../')
+            }
+        });
+    });
+});
 
-
-
-	describe('Something', function(){
-		it('should', function(){
-
-		});		
-	});
-	
+describe('Restore dump sql file to database', function () {
+    it('Should restore dump sql file', function () {
+        var tool = new PgTools();
+        tool.restoreDatabase({
+            host: 'localhost'
+            , port: 5432
+            , user: 'postgres'
+            , password: 'navalarti'
+            , sqlFilePath: '/home/naval/arunav1430762417616.sql'
+            , database: 'test'
+        }, function (error, output, message) {
+            if (error instanceof Error) {
+                console.log(code);
+            } else {
+                console.log(output);
+                console.log(message);
+            }
+        });
+    });
+});
