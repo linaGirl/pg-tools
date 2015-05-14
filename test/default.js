ee456c63eaf038c4b147e1682ab3223e1b152d17
naval <navalmca2009@gmail.com>
2015-05-14 20:46:01 +0530

    updated test code

--- test/default.js
+++ test/default.js
@@ -12,9 +12,9 @@ describe('Create dump sql file', function () {
             host: 'localhost'
             , port: 5432
             , user: 'postgres'
-            , password: 'navalarti'
-            , dumpPath: '/home/naval'
-            , database: 'arunav'
+            , password: ''
+            , dumpPath: '../'
+            , database: 'postgres'
         }, function (error, output, message, dumpFileName) {
             if (error instanceof Error) {
                 console.log(error);
@@ -22,28 +22,27 @@ describe('Create dump sql file', function () {
                 console.log(output);
                 console.log(message);
                 console.log(dumpFileName);
+                describe('Restore dump sql file to database', function () {
+                    it('Should restore dump sql file', function () {
+                        var tool = new PgTools();
+                        tool.restoreDatabase({
+                            host: 'localhost'
+                            , port: 5432
+                            , user: 'postgres'
+                            , password: ''
+                            , sqlFilePath: dumpFileName
+                            , database: 'test'
+                        }, function (error, output, message) {
+                            if (error instanceof Error) {
+                                console.log(error);
+                            } else {
+                                console.log(output);
+                                console.log(message);
+                            }
+                        });
+                    });
+                });
 
-            }
-        });
-    });
-});
-
-describe('Restore dump sql file to database', function () {
-    it('Should restore dump sql file', function () {
-        var tool = new PgTools();
-        tool.restoreDatabase({
-            host: 'localhost'
-            , port: 5432
-            , user: 'postgres'
-            , password: 'navalarti'
-            , sqlFilePath: '/home/naval/arunav1430762417616.sql'
-            , database: 'test'
-        }, function (error, output, message) {
-            if (error instanceof Error) {
-                console.log(error);
-            } else {
-                console.log(output);
-                console.log(message);
             }
         });
     });
