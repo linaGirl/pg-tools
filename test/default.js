1090478ef969c7a0145a1d264639413f0d205080
naval <navalmca2009@gmail.com>
2015-05-15 00:24:56 +0530

    improved test case and added es6 promise lib

--- test/default.js
+++ test/default.js
@@ -3,46 +3,42 @@
 var Class = require('ee-class')
         , log = require('ee-log')
         , assert = require('assert')
+        , os = require('os')
+        , Promise = require('es6-promise').Promise
         , PgTools = require('../');
 
-describe('Create dump sql file', function () {
+describe('Pg Tools', function () {
+    var dumpSqlFileName = '';
     it('Should create dump sql file', function () {
         var tool = new PgTools();
         tool.dumpDatabase({
             host: 'localhost'
             , port: 5432
             , user: 'postgres'
-            , password: 'navalarti'
-            , dumpPath: __dirname+'/..'
-            , database: 'arunav'
-        }, function (error, output, message, dumpFileName) {
+            , password: ''
+            , dumpPath: os.tmpdir()
+            , database: 'postgres'
+        }).then(function (error, output, message, dumpFileName) {
+            dumpSqlFileName = dumpFileName;
+
+        });
+    });
+
+    it('Should restore dump sql file', function () {
+        var tool = new PgTools();
+        tool.restoreDatabase({
+            host: 'localhost'
+            , port: 5432
+            , user: 'postgres'
+            , password: ''
+            , sqlFilePath: dumpSqlFileName
+            , database: 'test'
+        }, function (error, output, message) {
             if (error instanceof Error) {
                 console.log(error);
             } else {
                 console.log(output);
                 console.log(message);
-                console.log(dumpFileName);
-                describe('Restore dump sql file to database', function () {
-                    it('Should restore dump sql file', function () {
-                        var tool = new PgTools();
-                        tool.restoreDatabase({
-                            host: 'localhost'
-                            , port: 5432
-                            , user: 'postgres'
-                            , password: 'navalarti'
-                            , sqlFilePath: dumpFileName
-                            , database: 'test'
-                        }, function (error, output, message) {
-                            if (error instanceof Error) {
-                                console.log(error);
-                            } else {
-                                console.log(output);
-                                console.log(message);
-                            }
-                        });
-                    });
-                });
-
             }
         });
     });
